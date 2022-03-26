868382343c813c1fab44b2280b548906892294cf
DND-inc <frontend.work@icloud.com>
2022-03-26 20:09:11 +0300

    Rebase from Master

--- app/assets/js/modules/tabsCategoriesCard.js
+++ app/assets/js/modules/tabsCategoriesCard.js
@@ -1,46 +1,38 @@
-
-
-
 export function tabsCategoriesCard(triggers, contents, filter) {
-    
     const tabsTriggersDomElements = document.querySelectorAll(triggers),
         tabsContentsDomElelements = document.querySelectorAll(contents);
 
-       function hiddenAll() {
-            tabsContentsDomElelements.forEach(el => {
-                el.style.display = "none";
+    function hiddenAll() {
+        tabsContentsDomElelements.forEach((el) => {
+            el.style.display = "none";
+        });
+
+        tabsTriggersDomElements.forEach((el) => {
+            let colorTriggers = el.querySelectorAll(".svg-tab");
+            colorTriggers.forEach((el) => {
+                el.closest("li").classList.remove("active-tab");
+                el.style.fill = "#846653";
             });
+        });
+    }
 
-            tabsTriggersDomElements.forEach(el => {
-                let colorTriggers = el.querySelectorAll(".svg-tab");
-                colorTriggers.forEach((el)=>{
-                    el.closest("li").classList.remove("active-tab");
-                    el.style.fill = "#846653";
-                });
-            });
-       }
+    function showCurrent(triggerNum) {
+        let colorTriggers = tabsTriggersDomElements[triggerNum].querySelectorAll(".svg-tab");
+        tabsContentsDomElelements[triggerNum].style.display = "block";
+        colorTriggers.forEach((el) => {
+            el.closest("li").classList.add("active-tab");
+            el.style.fill = "#ffffff";
+        });
+    }
 
-        function showCurrent(triggerNum) {
-            let colorTriggers = tabsTriggersDomElements[triggerNum].querySelectorAll(".svg-tab");
-            tabsContentsDomElelements[triggerNum].style.display = "block";
-            colorTriggers.forEach((el)=>{
-                el.closest("li").classList.add("active-tab");
-                el.style.fill = "#ffffff";
-            });
-            
-       }
+    tabsTriggersDomElements.forEach((el, i) => {
+        el.addEventListener("click", (e) => {
+            hiddenAll();
+            showCurrent(i);
+            filter.renderCard(el);
+        });
+    });
 
-       
-
-       tabsTriggersDomElements.forEach((el, i) => {
-            el.addEventListener("click",(e)=>{
-                hiddenAll();
-                showCurrent(i);
-                filter.renderCard(el);
-            }); 
-       });
-
-       hiddenAll();
-       showCurrent(0);
-
+    hiddenAll();
+    showCurrent(0);
 }
