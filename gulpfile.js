cf92fa463ace268c701c83064587e92889ba8382
DND-dev <frontend.work@icloud.com>
2022-07-09 22:31:34 +0300

    modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,124 +1,139 @@
-let { src, dest, watch, parallel } = require('gulp'),
-	browserSync = require('browser-sync'),
-	// uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	rename = require('gulp-rename'),
-	pug = require('gulp-pug'),
-	autoprefixer = require('gulp-autoprefixer'),
-	sass = require('gulp-sass')(require('sass'));
-	
-const webpack = require("webpack-stream");
+let { src, dest, watch, parallel } = require("gulp"),
+    browserSync = require("browser-sync"),
+    // uglify = require('gulp-uglify'),
+    concat = require("gulp-concat"),
+    rename = require("gulp-rename"),
+    pug = require("gulp-pug"),
+    autoprefixer = require("gulp-autoprefixer"),
+    sass = require("gulp-sass")(require("sass"))
 
-const dist = "./dist/";
+const webpack = require("webpack-stream")
+
+const dist = "./dist/"
 // const dist = "/Applications/MAMP/htdocs/test/dist/";
 
-
 function gulpPug() {
-	return src('app/*.pug')
-		.pipe(pug({
-			pretty: true
-		}))
-		.pipe(dest('app'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-};
-
-
+    return src("app/*.pug")
+        .pipe(
+            pug({
+                pretty: true
+            })
+        )
+        .pipe(dest("app"))
+        .pipe(
+            browserSync.reload({
+                stream: true
+            })
+        )
+}
 
 function scss() {
-	return src('app/assets/scss/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}))
-		.pipe(autoprefixer({
-			overrideBrowserslist: ['last 8 versions']
-		}))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(dest('app/assets/css'))
-		.pipe(dest(dist+'/assets/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-};
+    return src("app/assets/scss/**/*.scss")
+        .pipe(
+            sass({
+                outputStyle: "compressed"
+            })
+        )
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 8 versions"]
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min"
+            })
+        )
+        .pipe(dest("app/assets/css"))
+        .pipe(dest(dist + "/assets/css"))
+        .pipe(
+            browserSync.reload({
+                stream: true
+            })
+        )
+}
 
-function  css() {
-	return src([
-		'node_modules/normalize.css/normalize.css',
-		'node_modules/slick-carousel/slick/slick.css',
-		//'node_modules/magnific-popup/dist/magnific-popup.css',
-		'node_modules/simplebar/dist/simplebar.min.css',
-		'node_modules/animate.css/animate.css',
-		//'node_modules/rateyo/src/jquery.rateyo.css',
-		//'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
-		//'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
-		//'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
-	])
-		.pipe(concat('_libs.scss'))
-		.pipe(dest('app/assets/scss'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-};
+function css() {
+    return src([
+        "node_modules/normalize.css/normalize.css",
+        "node_modules/slick-carousel/slick/slick.css",
+        //'node_modules/magnific-popup/dist/magnific-popup.css',
+        "node_modules/simplebar/dist/simplebar.min.css",
+        "node_modules/animate.css/animate.css"
+        //'node_modules/rateyo/src/jquery.rateyo.css',
+        //'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        //'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        //'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+    ])
+        .pipe(concat("_libs.scss"))
+        .pipe(dest("app/assets/scss"))
+        .pipe(
+            browserSync.reload({
+                stream: true
+            })
+        )
+}
 
 function html() {
-	return src('app/*.html')
-		.pipe(dest(dist))
-		.pipe(browserSync.stream());
-};
+    return src("app/*.html").pipe(dest(dist)).pipe(browserSync.stream())
+}
 
-function buildJs(){
+function buildJs() {
     return src("app/assets/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'bundle.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
+        .pipe(
+            webpack({
+                mode: "development",
+                output: {
+                    filename: "bundle.js"
+                },
+                watch: false,
+                devtool: "source-map",
+                module: {
+                    rules: [
+                        {
                             test: /\.m?js$/,
                             exclude: /(node_modules|bower_components)/,
                             use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    // debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
+                                loader: "babel-loader",
+                                options: {
+                                    presets: [
+                                        [
+                                            "@babel/preset-env",
+                                            {
+                                                // debug: true,
+                                                corejs: 3,
+                                                useBuiltIns: "usage"
+                                            }
+                                        ]
+                                    ]
+                                }
                             }
-                          }
-                        ]
-                      }
-				}))
-				.pipe(dest(dist + "assets/js"))
-                .on("end", browserSync.reload);
-};
+                        }
+                    ]
+                }
+            })
+        )
+        .pipe(dest(dist + "assets/js"))
+        .on("end", browserSync.reload)
+}
 
-function copyAssets(){
+function copyAssets() {
     return src(["./app/assets/**/*.*", "!app/assets/scss/**/*.*", "!app/assets/js/**/*.*"])
-                .pipe(dest(dist + "/assets"))
-                .on("end", browserSync.reload);
-};
+        .pipe(dest(dist + "/assets"))
+        .on("end", browserSync.reload)
+}
 
-function watching () {
-	browserSync.init({
-		server: dist,
-		port: 4000,
-		notify: true
-    });
-	watch('app/**/*.pug', parallel(gulpPug));
-	watch('app/**/*.html', parallel(html));
-	watch('app/assets/scss/**/*.scss', parallel(scss));
-	watch('app/assets/js/**/*.js', parallel(buildJs));
-};
-
+function watching() {
+    browserSync.init({
+        server: dist,
+        port: 4000,
+        notify: true
+    })
+    watch("app/**/*.pug", parallel(gulpPug))
+    watch("app/**/*.html", parallel(html))
+    watch("app/assets/scss/**/*.scss", parallel(scss))
+    watch("app/assets/js/**/*.js", parallel(buildJs))
+}
 
 // gulp.task ('images', function(){
 // 	return gulp.src('app/assets/img/**/*')
@@ -165,4 +180,4 @@ function watching () {
 // });
 // gulp.task('build', gulp.series("build-prod-js"));
 
-exports.default = parallel(gulpPug, html, css, buildJs, copyAssets, watching);
+exports.default = parallel(gulpPug, html, scss, css, buildJs, copyAssets, watching)
